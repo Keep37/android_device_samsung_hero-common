bff7585fba0c4d5f8ab423be5d1b71cc344df64b
Keep37 <keepdev37@gmail.com>
2020-12-28 19:36:08 +0100

    Add ubuntu touch components

--- hero-common.mk
+++ hero-common.mk
@@ -108,7 +108,47 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	dtbhtoolExynos
 
-# Inherit board specific products
+#####################
+### Ubuntu Touch
+#####################
+# Aethercast
+PRODUCT_PROPERTY_OVERRIDES += \
+  ubuntu.widi.supported=1
+
+PRODUCT_PACKAGES += \
+  libubuntu_application_api \
+  libcameraservice \
+  libdroidmedia \
+  libcamera_compat_layer \
+  libmedia_compat_layer \
+  libui_compat_layer \
+  libsf_compat_layer \
+  minimediaservice \
+  minisfservice \
+  libminisf \
+  libaudioflingerglue \
+  miniafservice
+
+# telepathy-ofono quirks
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.t-o.quirk.forcesink=sink.primary_output \
+  ro.t-o.quirk.forcesource=source.primary_input
+
+PRODUCT_COPY_FILES += \
+  $(LOCAL_PATH)/ubuntu/ofono.override:system/halium/etc/init/ofono.override \
+  $(LOCAL_PATH)/ubuntu/timekeeper.conf:system/halium/etc/init/timekeeper.conf \
+  $(LOCAL_PATH)/ubuntu/android-tools-adb.conf:system/halium/etc/init/android-tools-adb.conf \
+  $(LOCAL_PATH)/ubuntu/apparmor/abstractions/base:system/halium/etc/apparmor.d/abstractions/base \
+  $(LOCAL_PATH)/ubuntu/apparmor/local/usr.bin.media-hub-server:system/halium/etc/apparmor.d/local/usr.bin.media-hub-server \
+  $(LOCAL_PATH)/ubuntu/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/video.d/apparmor-easyprof-ubuntu_android \
+  $(LOCAL_PATH)/ubuntu/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android:system/halium/usr/share/apparmor/hardware/graphics.d/apparmor-easyprof-ubuntu_android \
+  $(LOCAL_PATH)/ubuntu/android.conf:system/halium/etc/ubuntu-touch-session.d/android.conf \
+  $(LOCAL_PATH)/ubuntu/touch.pa:system/halium/etc/pulse/touch.pa \
+  $(LOCAL_PATH)/ubuntu/70-android.rules:system/halium/lib/udev/rules.d/70-android.rules
+
+######## End Ubuntu Touch ##########
+
+# Inherit board specific productss
 -include $(LOCAL_PATH)/product/*.mk
 
 # System properties
